d8d468f60e5d30a4a716d6d6f56f5a33edb3afc4
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-01 16:35:11 -0700

    Final commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const expressPort = 3000;
+
+
+// Serial port comm
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM3', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+// Read the port data
+port.on("open", () => {
+    console.log('Serial communication is being started.');
+});
+parser.on('data', data => {
+    console.log(data);
+});
+
+// 
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile('public/index.html');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    port.write(req.body.led, err => {
+    });
+});
+
+app.listen(expressPort, () => { console.log("Listening on port 3000") });
---
+++ arduino/led_circuit.ino
@@ -0,0 +1,21 @@
+const byte led = 3;
+int counter = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  if(Serial.available() > 0){
+    char lectura = Serial.read();
+    if(lectura == '1'){
+      Serial.println(lectura);
+      digitalWrite(led, HIGH);
+    }else if(lectura == '0'){
+      Serial.println(lectura);
+      digitalWrite(led, LOW);
+    }
+  }
+
+}
